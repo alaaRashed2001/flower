5a26c93f0a25f827e30235939d1766f646de5959
AlaaNomanRashed <114382094+AlaaNomanRashed@users.noreply.github.com>
2022-12-12 13:04:51 +0200

    lincking firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flower/provider/cart_provider.dart';
 import 'package:flower/views/screens/auth/login_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp( MyApp());
 }
 
